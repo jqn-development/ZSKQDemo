67f9d1964c8d9ea71c7d7ebb25e583bcdcdfbba3
emily <435477171@qq.com>
2016-06-03 09:30:29 +0800

    EStyleSheetDemo添加样式

--- app/EStyleSheetDemo.js
+++ app/EStyleSheetDemo.js
@@ -6,33 +6,69 @@ import {
   View,
   DeviceEventEmitter,
   TouchableOpacity,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  Dimensions
 } from 'react-native';
 
 // component.js
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-// calc styles
-EStyleSheet.build({
-  textColor: 'black'
-});
+// app entry
+import theme from './style/theme';
+EStyleSheet.build(theme);
+
+
+// // calc styles
+// let {height,width} = Dimensions.get('window');
+// EStyleSheet.build({
+//   rem: width > 340 ? 18 : 16
+// });
 
 const styles = EStyleSheet.create({
+  $columnWidth: '80%',
   column: {
-    width: '80%'         // 80% of screen width
+    width: '$columnWidth',
+    flexDirection: 'row'
+  },
+  subColumnLeft: {
+    width: '0.3 * $columnWidth'
+  },
+  subColumnRight: {
+    width: '0.7 * $columnWidth'
   },
   text: {
-    color: '$textColor', // use variable $textColor
-    fontSize: '1.5rem'   // use relative unit - CSS3 rem
+    color: '$textColor',
+    fontSize: '1.5rem'
+  },
+  btn:{
+    color: '$button.color',
+    fontSize: '$button.size',
+  },
+  header:{
+    fontSize:18,
+    '@media ios':{
+      color:'green',
+    },
+    '@media android':{
+      color:'blue',
+    },
   }
 });
 
 export default class EStyleSheetDemo extends React.Component {
+
   render() {
     return (
-      // use styles as usual
-      <View style={styles.column}>
-        <Text style={styles.text}>Hello</Text>
+      <View style={{flex:1}}>
+        <View style={styles.column}>
+          <Text style={styles.text}>Hello</Text>
+          <Text style={styles.btn}>Button</Text>
+          <Text style={styles.header}>I should be blue</Text>
+        </View>
+        <View style={styles.column}>
+            <View style={styles.subColumnLeft}><Text>fasd</Text></View>
+            <View style={styles.subColumnRight}><Text>fasd</Text></View>
+          </View>
       </View>
     );
   }
---
+++ app/style/theme.js
@@ -0,0 +1,8 @@
+export default{
+  textColor: '#ff0000',
+  buttonColor: 'green',
+  button:{
+    color: '#00ff00',
+    size: 30,
+  }
+}
